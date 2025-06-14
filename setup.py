2f78b79f1cb44cd22f376e3c763767542ef0e473
Chetana Rane <chetanarane10@gmail.com>
2025-06-15 03:00:00 +0530

    Adding the README File

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import find_packages,setup
+from typing import List
 
+HyPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for re in requirements]
+
+        if HyPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
 name='ML_Project',
@@ -7,6 +22,6 @@ version='0.0.1',
 author='Chetana',
 author_email='chetanarane10@gmail.com',
 packages=find_packages(),
-install_requires=['pandas','numpy','seaborn']
+install_requires=get_requirements['requirement.txt']
 
 )
