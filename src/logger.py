6a000fb3e0e423cdf0408b5db2e7ebf28a01af81
Chetana Rane <chetanarane10@gmail.com>
2025-06-21 03:00:00 +0530

    Adding the File

--- src/logger.py
+++ src/logger.py
@@ -2,23 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# Create a unique log file name with a timestamp
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Define the path for the logs directory
-logs_path = os.path.join(os.getcwd(), "logs")
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Create the logs directory if it doesn't exist
-os.makedirs(logs_path, exist_ok=True)
-
-# Define the full path for the log file
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-
-# Configure the basic logging settings
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    force=True # This will re-apply the configuration even if handlers already exist
-)
 
+
+)
